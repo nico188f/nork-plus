c4e742c0fd4f815aae6efec91129b86548a82d3b
Nicolaj Blach Jensen <nico188f@gmail.com>
2025-07-28 18:42:38 +0200

    fixed type names

--- src/api/booking/getBookings.ts
+++ src/api/booking/getBookings.ts
@@ -5,10 +5,10 @@ import type { RoomBooking } from "@/typesAndSchemas/RoomBookings";
 import type { ResourceId } from "@/typesAndSchemas/Room";
 import { RoomBookingSchema } from "@/typesAndSchemas/RoomBookings";
 
-type roomListType = Array<{ id: ResourceId }>;
+type RoomList = Array<{ id: ResourceId }>;
 
-type BookingRequest = {
-   roomList: roomListType;
+type GetBookingParams = {
+   roomList: RoomList;
    from: Date;
    to: Date;
 };
@@ -18,13 +18,13 @@ const apiResource = "publicBooking/public/getBookings";
 const Data = z.array(RoomBookingSchema);
 
 export default async (
-   bookingRequest: BookingRequest,
+   getBookingParams: GetBookingParams,
 ): Promise<Array<RoomBooking>> => {
    const res = await axios.post(norkApi.url + apiResource, {
       organization: norkApi.org,
-      resourceList: bookingRequest.roomList,
-      from: bookingRequest.from.toISOString().slice(0, 19),
-      to: bookingRequest.to.toISOString().slice(0, 19),
+      resourceList: getBookingParams.roomList,
+      from: getBookingParams.from.toISOString().slice(0, 19),
+      to: getBookingParams.to.toISOString().slice(0, 19),
    });
 
    const data = Data.parse(res.data);
